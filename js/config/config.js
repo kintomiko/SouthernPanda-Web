aaacbf4f2edb59d6d61df37842e5b373617c8e0a
Kin Lin <kin.lin@sparkventures.co.nz>
2016-09-08 22:18:44 +1200

    support update profile and avatar add home page

--- js/config/config.js
+++ js/config/config.js
@@ -3,7 +3,7 @@
  */
 
 
-app.constant('SERVER_URL', 'http://127.0.0.1:8080');
-//app.constant('SERVER_URL', 'http://45.55.250.128:8080');
+//app.constant('SERVER_URL', 'http://127.0.0.1:8080');
+app.constant('SERVER_URL', 'http://45.55.250.128:8080');
 app.constant('LOGIN_API', '/user/search/findUserByEmail?email=');
 app.constant('ITEM_API', '/item');
